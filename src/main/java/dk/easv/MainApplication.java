f0ed80d27849429617d5dbf565fad46209c7e93b
Valeri Krebs <118070044+val-krebs@users.noreply.github.com>
2026-04-01 16:13:29 +0200

    Established connection with DB

--- src/main/java/dk/easv/MainApplication.java
+++ src/main/java/dk/easv/MainApplication.java
@@ -1,15 +1,26 @@
 package dk.easv;
 
+import dk.easv.dal.ConnectionManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+
 public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        /*try {
+            Connection conn = new ConnectionManager().getConnection();
+            System.out.println("Connected!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
         FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("/dk/easv/ticket_gui_only/LoginView.fxml"));
         Parent root = loader.load();
 
